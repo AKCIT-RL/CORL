1ec15d6dba09799591f4538b988dbbd18817f2e9
luanagbmartins <luanagbmartins@gmail.com>
2025-05-20 10:28:41 -0300

    Add new YAML configuration files for SAC-N and CQL training across various environments. Update replay buffer implementation to ensure rewards and terminals are wrapped in tensors. Refactor script files for improved organization and clarity.

--- scripts/cql.sh
+++ scripts/cql.sh
@@ -1,1 +1,35 @@
-python -m algorithms.offline.cql --config_path="configs/offline/cql/go1/joystick_flat_expert.yaml" --device "cuda:0" --normalize "false" --normalize_reward "true" --reward_scale "1.0" --checkpoints_path "checkpoints/cql"
+python -m algorithms.offline.cql --config_path="configs/offline/cql/go1/joystick_flat_expert.yaml"  --checkpoints_path "checkpoints/CQL"
+python -m algorithms.offline.cql --config_path="configs/offline/cql/go1/joystick_flat_medium.yaml" --checkpoints_path "checkpoints/CQL"
+# python -m algorithms.offline.cql --config_path="configs/offline/cql/go1/joystick_flat_random.yaml" --checkpoints_path "checkpoints/CQL"
+
+python -m algorithms.offline.cql --config_path="configs/offline/cql/go1/joystick_rough_expert.yaml" --checkpoints_path "checkpoints/CQL"
+python -m algorithms.offline.cql --config_path="configs/offline/cql/go1/joystick_rough_medium.yaml" --checkpoints_path "checkpoints/CQL"
+# python -m algorithms.offline.cql --config_path="configs/offline/cql/go1/joystick_rough_random.yaml" --checkpoints_path "checkpoints/CQL"
+
+python -m algorithms.offline.cql --config_path="configs/offline/cql/go1/handstand_expert.yaml" --checkpoints_path "checkpoints/CQL"
+python -m algorithms.offline.cql --config_path="configs/offline/cql/go1/handstand_medium.yaml" --checkpoints_path "checkpoints/CQL"
+# python -m algorithms.offline.cql --config_path="configs/offline/cql/go1/handstand_random.yaml" --checkpoints_path "checkpoints/CQL"
+
+python -m algorithms.offline.cql --config_path="configs/offline/cql/go1/getup_expert.yaml" --checkpoints_path "checkpoints/CQL"
+python -m algorithms.offline.cql --config_path="configs/offline/cql/go1/getup_medium.yaml" --checkpoints_path "checkpoints/CQL"
+# python -m algorithms.offline.cql --config_path="configs/offline/cql/go1/getup_random.yaml" --checkpoints_path "checkpoints/CQL"
+
+python -m algorithms.offline.cql --config_path="configs/offline/cql/go1/footstand_expert.yaml" --checkpoints_path "checkpoints/CQL"
+python -m algorithms.offline.cql --config_path="configs/offline/cql/go1/footstand_medium.yaml" --checkpoints_path "checkpoints/CQL"
+# python -m algorithms.offline.cql --config_path="configs/offline/cql/go1/footstand_random.yaml" --checkpoints_path "checkpoints/CQL"
+
+python -m algorithms.offline.cql --config_path="configs/offline/cql/g1/joystick_flat_expert.yaml" --checkpoints_path "checkpoints/CQL"
+python -m algorithms.offline.cql --config_path="configs/offline/cql/g1/joystick_flat_medium.yaml" --checkpoints_path "checkpoints/CQL"
+# python -m algorithms.offline.cql --config_path="configs/offline/cql/g1/joystick_flat_random.yaml" --checkpoints_path "checkpoints/CQL"
+
+python -m algorithms.offline.cql --config_path="configs/offline/cql/g1/joystick_rough_expert.yaml" --checkpoints_path "checkpoints/CQL"
+python -m algorithms.offline.cql --config_path="configs/offline/cql/g1/joystick_rough_medium.yaml" --checkpoints_path "checkpoints/CQL"
+# python -m algorithms.offline.cql --config_path="configs/offline/cql/g1/joystick_rough_random.yaml" --checkpoints_path "checkpoints/CQL"
+
+python -m algorithms.offline.cql --config_path="configs/offline/cql/h1/inplace_gait_expert.yaml" --checkpoints_path "checkpoints/CQL"
+python -m algorithms.offline.cql --config_path="configs/offline/cql/h1/inplace_gait_medium.yaml" --checkpoints_path "checkpoints/CQL"
+# python -m algorithms.offline.cql --config_path="configs/offline/cql/h1/inplace_gait_random.yaml" --checkpoints_path "checkpoints/CQL"
+
+python -m algorithms.offline.cql --config_path="configs/offline/cql/h1/joystick_gait_expert.yaml" --checkpoints_path "checkpoints/CQL"
+python -m algorithms.offline.cql --config_path="configs/offline/cql/h1/joystick_gait_medium.yaml" --checkpoints_path "checkpoints/CQL"
+# python -m algorithms.offline.cql --config_path="configs/offline/cql/h1/joystick_gait_random.yaml" --checkpoints_path "checkpoints/CQL"
