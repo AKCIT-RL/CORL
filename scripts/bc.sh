a7ab8c3b16e29f685254afb7127464fb4529576b
luanagbmartins <luanagbmartins@gmail.com>
2025-06-23 10:44:44 -0300

    Update Jupyter notebooks for expert and offline evaluations; add new manipulation notebook and enhance result handling.

--- scripts/bc.sh
+++ scripts/bc.sh
@@ -6,20 +6,24 @@
 # python -m algorithms.offline.any_percent_bc --config_path="configs/offline/bc/go1/joystick_rough_medium.yaml" --device "cuda:0" --normalize "false" --checkpoints_path "checkpoints/BC"
 # python -m algorithms.offline.any_percent_bc --config_path="configs/offline/bc/go1/joystick_rough_random.yaml" --device "cuda:0" --normalize "false" --checkpoints_path "checkpoints/BC"
 
-python -m algorithms.offline.any_percent_bc --config_path="configs/offline/bc/go1/handstand_expert.yaml" --device "cuda:0" --normalize "false" --checkpoints_path "checkpoints/BC"
-python -m algorithms.offline.any_percent_bc --config_path="configs/offline/bc/go1/handstand_medium.yaml" --device "cuda:0" --normalize "false" --checkpoints_path "checkpoints/BC"
+# python -m algorithms.offline.any_percent_bc --config_path="configs/offline/bc/go1/handstand_expert.yaml" --device "cuda:0" --normalize "false" --checkpoints_path "checkpoints/BC"
+# python -m algorithms.offline.any_percent_bc --config_path="configs/offline/bc/go1/handstand_medium.yaml" --device "cuda:0" --normalize "false" --checkpoints_path "checkpoints/BC"
 # python -m algorithms.offline.any_percent_bc --config_path="configs/offline/bc/go1/handstand_random.yaml" --device "cuda:0" --normalize "false" --checkpoints_path "checkpoints/BC"
 
-python -m algorithms.offline.any_percent_bc --config_path="configs/offline/bc/go1/footstand_expert.yaml" --device "cuda:0" --normalize "false" --checkpoints_path "checkpoints/BC"
+# python -m algorithms.offline.any_percent_bc --config_path="configs/offline/bc/go1/getup_expert.yaml" --device "cuda:0" --normalize "false" --checkpoints_path "checkpoints/BC"
+# python -m algorithms.offline.any_percent_bc --config_path="configs/offline/bc/go1/getup_medium.yaml" --device "cuda:0" --normalize "false" --checkpoints_path "checkpoints/BC"
+# python -m algorithms.offline.any_percent_bc --config_path="configs/offline/bc/go1/getup_random.yaml" --device "cuda:0" --normalize "false" --checkpoints_path "checkpoints/BC"
+
+# python -m algorithms.offline.any_percent_bc --config_path="configs/offline/bc/go1/footstand_expert.yaml" --device "cuda:0" --normalize "false" --checkpoints_path "checkpoints/BC"
 # python -m algorithms.offline.any_percent_bc --config_path="configs/offline/bc/go1/footstand_medium.yaml" --device "cuda:0" --normalize "false" --checkpoints_path "checkpoints/BC"
 # python -m algorithms.offline.any_percent_bc --config_path="configs/offline/bc/go1/footstand_random.yaml" --device "cuda:0" --normalize "false" --checkpoints_path "checkpoints/BC"
 
-python -m algorithms.offline.any_percent_bc --config_path="configs/offline/bc/g1/joystick_flat_expert.yaml" --device "cuda:0" --normalize "false" --checkpoints_path "checkpoints/BC"
-# python -m algorithms.offline.any_percent_bc --config_path="configs/offline/bc/g1/joystick_flat_medium.yaml" --device "cuda:0" --normalize "false" --checkpoints_path "checkpoints/BC"
+# python -m algorithms.offline.any_percent_bc --config_path="configs/offline/bc/g1/joystick_flat_expert.yaml" --device "cuda:0" --normalize "false" --checkpoints_path "checkpoints/BC"
+python -m algorithms.offline.any_percent_bc --config_path="configs/offline/bc/g1/joystick_flat_medium.yaml" --device "cuda:0" --normalize "false" --checkpoints_path "checkpoints/BC"
 # python -m algorithms.offline.any_percent_bc --config_path="configs/offline/bc/g1/joystick_flat_random.yaml" --device "cuda:0" --normalize "false" --checkpoints_path "checkpoints/BC"
 
-python -m algorithms.offline.any_percent_bc --config_path="configs/offline/bc/g1/joystick_rough_expert.yaml" --device "cuda:0" --normalize "false" --checkpoints_path "checkpoints/BC"
-# python -m algorithms.offline.any_percent_bc --config_path="configs/offline/bc/g1/joystick_rough_medium.yaml" --device "cuda:0" --normalize "false" --checkpoints_path "checkpoints/BC"
+# python -m algorithms.offline.any_percent_bc --config_path="configs/offline/bc/g1/joystick_rough_expert.yaml" --device "cuda:0" --normalize "false" --checkpoints_path "checkpoints/BC"
+python -m algorithms.offline.any_percent_bc --config_path="configs/offline/bc/g1/joystick_rough_medium.yaml" --device "cuda" --normalize "false" --checkpoints_path "checkpoints/BC"
 # python -m algorithms.offline.any_percent_bc --config_path="configs/offline/bc/g1/joystick_rough_random.yaml" --device "cuda:0" --normalize "false" --checkpoints_path "checkpoints/BC"
 
 # python -m algorithms.offline.any_percent_bc --config_path="configs/offline/bc/h1/inplace_gait_expert.yaml" --device "cuda:0" --normalize "false" --checkpoints_path "checkpoints/BC"
--- scripts/td3bc.sh
+++ scripts/td3bc.sh
@@ -6,12 +6,12 @@
 # python -m algorithms.offline.td3_bc --config_path="configs/offline/td3_bc/go1/joystick_rough_medium.yaml" --device "cuda:0" --normalize "false" --checkpoints_path "checkpoints/TD3-BC"
 # python -m algorithms.offline.td3_bc --config_path="configs/offline/td3_bc/go1/joystick_rough_random.yaml" --device "cuda:0" --normalize "false" --checkpoints_path "checkpoints/TD3-BC"
 
-python -m algorithms.offline.td3_bc --config_path="configs/offline/td3_bc/go1/handstand_expert.yaml" --device "cuda:0" --normalize "false" --checkpoints_path "checkpoints/TD3-BC"
-python -m algorithms.offline.td3_bc --config_path="configs/offline/td3_bc/go1/handstand_medium.yaml" --device "cuda:0" --normalize "false" --checkpoints_path "checkpoints/TD3-BC"
+# python -m algorithms.offline.td3_bc --config_path="configs/offline/td3_bc/go1/handstand_expert.yaml" --device "cuda:0" --normalize "false" --checkpoints_path "checkpoints/TD3-BC"
+# python -m algorithms.offline.td3_bc --config_path="configs/offline/td3_bc/go1/handstand_medium.yaml" --device "cuda:0" --normalize "false" --checkpoints_path "checkpoints/TD3-BC"
 # python -m algorithms.offline.td3_bc --config_path="configs/offline/td3_bc/go1/handstand_random.yaml" --device "cuda:0" --normalize "false" --checkpoints_path "checkpoints/TD3-BC"
 
-python -m algorithms.offline.td3_bc --config_path="configs/offline/td3_bc/go1/footstand_expert.yaml" --device "cuda:0" --normalize "false" --checkpoints_path "checkpoints/TD3-BC"
-# python -m algorithms.offline.td3_bc --config_path="configs/offline/td3_bc/go1/footstand_medium.yaml" --device "cuda:0" --normalize "false" --checkpoints_path "checkpoints/TD3-BC"
+# python -m algorithms.offline.td3_bc --config_path="configs/offline/td3_bc/go1/footstand_expert.yaml" --device "cuda:0" --normalize "false" --checkpoints_path "checkpoints/TD3-BC"
+python -m algorithms.offline.td3_bc --config_path="configs/offline/td3_bc/go1/footstand_medium.yaml" --device "cuda:0" --normalize "false" --checkpoints_path "checkpoints/TD3-BC"
 # python -m algorithms.offline.td3_bc --config_path="configs/offline/td3_bc/go1/footstand_random.yaml" --device "cuda:0" --normalize "false" --checkpoints_path "checkpoints/TD3-BC"
 
 python -m algorithms.offline.td3_bc --config_path="configs/offline/td3_bc/g1/joystick_flat_expert.yaml" --device "cuda:0" --normalize "false" --checkpoints_path "checkpoints/TD3-BC"
@@ -22,10 +22,10 @@ python -m algorithms.offline.td3_bc --config_path="configs/offline/td3_bc/g1/joy
 python -m algorithms.offline.td3_bc --config_path="configs/offline/td3_bc/g1/joystick_rough_medium.yaml" --device "cuda:0" --normalize "false" --checkpoints_path "checkpoints/TD3-BC"
 # python -m algorithms.offline.td3_bc --config_path="configs/offline/td3_bc/g1/joystick_rough_random.yaml" --device "cuda:0" --normalize "false" --checkpoints_path "checkpoints/TD3-BC"
 
-python -m algorithms.offline.td3_bc --config_path="configs/offline/td3_bc/h1/inplace_gait_expert.yaml" --device "cuda:0" --normalize "false" --checkpoints_path "checkpoints/TD3-BC"
-python -m algorithms.offline.td3_bc --config_path="configs/offline/td3_bc/h1/inplace_gait_medium.yaml" --device "cuda:0" --normalize "false" --checkpoints_path "checkpoints/TD3-BC"
+# python -m algorithms.offline.td3_bc --config_path="configs/offline/td3_bc/h1/inplace_gait_expert.yaml" --device "cuda:0" --normalize "false" --checkpoints_path "checkpoints/TD3-BC"
+# python -m algorithms.offline.td3_bc --config_path="configs/offline/td3_bc/h1/inplace_gait_medium.yaml" --device "cuda:0" --normalize "false" --checkpoints_path "checkpoints/TD3-BC"
 # python -m algorithms.offline.td3_bc --config_path="configs/offline/td3_bc/h1/inplace_gait_random.yaml" --device "cuda:0" --normalize "false" --checkpoints_path "checkpoints/TD3-BC"
 
-python -m algorithms.offline.td3_bc --config_path="configs/offline/td3_bc/h1/joystick_gait_expert.yaml" --device "cuda:0" --normalize "false" --checkpoints_path "checkpoints/TD3-BC"
-python -m algorithms.offline.td3_bc --config_path="configs/offline/td3_bc/h1/joystick_gait_medium.yaml" --device "cuda:0" --normalize "false" --checkpoints_path "checkpoints/TD3-BC"
+# python -m algorithms.offline.td3_bc --config_path="configs/offline/td3_bc/h1/joystick_gait_expert.yaml" --device "cuda:0" --normalize "false" --checkpoints_path "checkpoints/TD3-BC"
+# python -m algorithms.offline.td3_bc --config_path="configs/offline/td3_bc/h1/joystick_gait_medium.yaml" --device "cuda:0" --normalize "false" --checkpoints_path "checkpoints/TD3-BC"
 # python -m algorithms.offline.td3_bc --config_path="configs/offline/td3_bc/h1/joystick_gait_random.yaml" --device "cuda:0" --normalize "false" --checkpoints_path "checkpoints/TD3-BC"
